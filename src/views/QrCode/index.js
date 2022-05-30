0d5db542458a5bead57c1f61f52715e5bc227dcd
William <willrodriguess77@gmail.com>
2022-05-30 15:57:34 -0300

    Criando pasta views e paginas: home, task e qrcode

---
+++ src/views/QrCode/index.js
@@ -0,0 +1,11 @@
+import * as S from './style'
+
+function QrCode(){
+    return(
+        <>
+        
+        </>
+    )
+}
+
+export default QrCode
---
+++ src/views/Task/index.js
@@ -0,0 +1,11 @@
+import * as S from './style'
+
+function Task(){
+    return(
+        <>
+        
+        </>
+    )
+}
+
+export default Task
