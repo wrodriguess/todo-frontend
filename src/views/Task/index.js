12cbe486f166cb068a593a9435b0d24c58eccfad
William <willrodriguess77@gmail.com>
2022-05-31 20:24:12 -0300

    Criando e estilizando a página Task

--- src/views/Task/index.js
+++ src/views/Task/index.js
@@ -1,11 +1,69 @@
-import * as S from './style'
+import {useState, useEffect} from 'react'
 
-function Task(){
-    return(
-        <>
-        
-        </>
-    )
+import * as S from './styles.js'
+import api from '../../services/api'
+import Header from '../../components/Header'
+import Footer from '../../components/Footer'
+import typeIcons from '../../utils/typeIcons'
+
+function Task() {
+  const [type, setType] = useState()
+  const [done, setDone] = useState(false)
+
+  return (
+    <S.Container>
+      <Header/>
+      
+      <S.Form>
+        <S.TypeIcons>
+          {typeIcons.map((icon, index) => (
+            // Impedindo de exibir a posição 0 do array
+            index > 0 && 
+              <button type="button" onClick={() => setType(index)}>
+                <img src={icon} alt="Tipo da tarefa" className={type && type !== index && 'inative'}/>
+              </button>
+          ))}
+        </S.TypeIcons>
+
+        <S.Input>
+          <span>Título</span>
+          <input type="text" placeholder="Título da tarefa"/>
+        </S.Input>
+
+        <S.TextArea>
+          <span>Descrição</span>
+          <textarea rows={5} placeholder="Descrição da tarefa"/>
+        </S.TextArea>
+
+        <S.Input>
+          <span>Data</span>
+          <input type="date"/>
+        </S.Input>
+
+        <S.Input>
+          <span>Hora</span>
+          <input type="time"/>
+        </S.Input>
+
+        <S.Options>
+          <div>
+            <input type="checkbox" id="done" checked={done} onChange={() => setDone(!done)}/>
+            <label htmlFor="done">CONCLUÍDO</label>
+          </div>
+          
+          <button type="button">EXCLUIR</button>
+        </S.Options>
+
+        <S.Save>
+          <button type="button">SALVAR</button>
+        </S.Save>
+
+      </S.Form>
+
+      <Footer/>    
+    </S.Container>
+  );
 }
-
-export default Task
+  
+export default Task;
+  
--- src/views/Task/styles.js
+++ src/views/Task/styles.js
@@ -1,1 +1,135 @@
 import styled from 'styled-components'
+
+export const Container = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+export const Form = styled.div`
+    width: 50%;
+    margin-bottom: 70px;
+`
+    
+export const TypeIcons = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+
+    img{
+        width: 50px;
+        height: 50px;
+        margin: 10px;
+        cursor: pointer;
+
+        &:hover{
+            opacity: 0.5;
+            transition: all 0.3s ease;
+        }
+    }
+
+    button{
+        background: none;
+        border: none;
+    }
+
+    .inative{
+        opacity: 0.5;
+    }
+`
+
+export const Input = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    margin: 20px 0;
+
+    span{
+        color: #707070;
+        margin: 5px 0;
+    }
+
+    input{
+        font-size: 16px;
+        padding: 15px;
+        border: none;
+        border-bottom: 1px solid #ee6b26;
+    }
+`
+
+export const TextArea = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    margin: 20px 0;
+
+    span{
+        color: #707070;
+        margin: 5px 0;
+    }
+
+    textarea{
+        resize: none;
+        font-size: 16px;
+        border: 1px solid #ee6b26;
+        border-radius: 5px;
+        padding: 10px;
+    }
+`
+
+export const Options = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    font-weight: bold;
+
+    input, label{
+        color: #ee6b26;
+        cursor: pointer;
+        font-size: 16px;
+
+        &:hover{
+            opacity: 0.5;
+            transition: all 0.3s ease;
+        }
+    }
+
+    button{
+        background: none;
+        border: none;
+        font-size: 16px;
+        cursor: pointer;
+        color: #20295F;
+        font-weight: bold;
+
+        &:hover{
+            opacity: 0.5;
+            transition: all 0.3s ease;
+        }
+    }
+`
+
+export const Save = styled.div`
+    width: 100%;
+    margin-top: 20px;
+
+    button{
+        width: 100%;
+        background: #ee6b26;
+        border: none;
+        font-size: 20px;
+        color: #fff;
+        font-weight: bold;
+        padding: 20px;
+        border-radius: 30px;
+        cursor: pointer;
+
+        &:hover{
+            opacity: 0.5;
+            transition: all 0.3s ease;
+        }
+    }
+
+
+`
